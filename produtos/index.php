e6c548ba6f8200c1a4f8850cb3d039c917a30b3d
Marco Antônio Ribeiro de Toledo <mardt@uol.com.br>
2018-07-31 07:35:15 -0300

    produtos

--- produtos/index.php
+++ produtos/index.php
@@ -7,25 +7,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bodyCriadores.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../bodyHome.css">
+    <link rel="stylesheet" href="../style.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Produtos</title>
 </head>
 <body>
     <header>ECO RING</header>
     <div id="menuInicial" class="menuInit">
-        <a href="index.html">Home</a>
+        <a href="../index.html">Home</a>
         <a href="#">Produtos</a>
-        <a href="criadores.html" class="ativo">Criadores</a>
-        <a href="contato.html">Contato</a>
+        <a href="../criadores.html" class="ativo">Criadores</a>
+        <a href="../contato.html">Contato</a>
         <a class="icone" id="iconeMenu">▼</a>
     </div>
     <div id="produtos">
         jasbdfubasdgfsda
+        <br>
+        jasbdfubasdgfsda
+        <br>
+        jasbdfubasdgfsda
+        <br>
+        jasbdfubasdgfsda
+        <br>
+        jasbdfubasdgfsda
+        <br>
+        jasbdfubasdgfsda
+        <br>
+        jasbdfubasdgfsda
+        <br>
+        jasbdfubasdgfsda
+        <br>
+        jasbdfubasdgfsda
+        <br>
+        jasbdfubasdgfsda
+        <br>
+        jasbdfubasdgfsda
+        <br>
+        jasbdfubasdgfsda
+        <br>
+        jasbdfubasdgfsda
+        <br>
+        jasbdfubasdgfsda
+        <br>
+        jasbdfubasdgfsda
+        <br>
     </div>
     <div class="rodape">Texto rodapé</div>
-    <script src="app.js"></script>
+    <script src="../app.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </body>
 </html>
