516082675652d4111b00b22e285ff7c8eb970ebb
Ocramoi <marcoantonioribeirodetoledo@gmail.com>
2020-03-28 17:00:43 -0300

    siteBcc

---
+++ app.js
@@ -0,0 +1,50 @@
+$(document).ready(() =>
+{
+    $("#inPesq").text("");
+    atualiza("");
+    atvs();
+});
+
+$("#inSim").on("click", e =>
+{
+    atualiza($("#inPesq").val());
+});
+
+$("#inPesq").on("keydown", e =>
+{
+    atualiza($("#inPesq").val());
+});
+
+function atualiza(txtPesquisa)
+{
+    $("#exibe").empty();
+    var arPesq = [];
+    arPesq = professores.filter(el => el.materia.toLowerCase().includes(txtPesquisa.toLowerCase()) || el.nome.toLowerCase().includes(txtPesquisa.toLowerCase()));
+    if(arPesq.length != 0)
+    {
+        for(mat in arPesq)
+        {
+            $('#exibe').append( "<div class='elExibe'>"+
+                                    "<div id='elMateria'><b>Matéria:</b> " + professores[mat].materia + "</div>" +
+                                    "<div id='elProf'><b>Professor:</b> " + professores[mat].nome + "</div>" +
+                                    "<div id='elSala'><b>Sala:</b> " + professores[mat].sala + "</div>" +
+                                    "<div id='elEmail'><b>Email:</b> <a href='mailto:'" + professores[mat].email + "' target='_blank'>" + professores[mat].email + "</a></div>" +
+                                    "<div id='elOnline'><b>Aulas online:</b> " + professores[mat].online + "</div>" +
+                                "</div>");
+        }
+    }
+    else
+    {
+        $('#exibe').append("<div class='elExibe'><b size='1.5rem'>Nenhuma matéria/professor encontrado</b></div>")
+    }
+}
+
+function atvs()
+{
+    var atv = 0;
+    for(atv; atv < atividades.length - 1; atv++)
+    {
+        $('#atvsExibe').append("<div class='elAtividade'><b>Data: </b>" + atividades[atv].dia + "<br><b>Atividade: </b>" + atividades[atv].atividade + "</div><hr>");
+    }
+    $('#atvsExibe').append("<div class='elAtividade'><b>Data: </b>" + atividades[atv].dia + "<br><b>Atividade: </b>" + atividades[atv].atividade + "</div>");
+}
---
+++ infos.js
@@ -0,0 +1,101 @@
+var professores = [
+    { 
+        materia: "Introdução à Ciência de Computação I",
+        nome: "Rodrigo Fernandes de Mello", 
+        sala: "3-242",
+        email: "mello@icmc.usp.br",
+        online: "Aulas: Segunda (10h) e Quinta (10h) no <a href='https://www.youtube.com/user/mellorf'>YouTube</a><br>" +
+                "<a href='https://ae4.tidia-ae.usp.br/portal'>Tidia</a><br>" +
+                "<a href='https://chat.whatsapp.com/LaWaWzMgEA7IwtLRjuvocN'>Grupo de WhatsApp</a>"
+    },
+    { 
+        materia: "Evolução Histórica da Computação e Aplicações",
+        nome: "Danilo Hernane Spatti", 
+        sala: "4-127",
+        email: "spatti@icmc.usp.br",
+        online: "Atendimento: Segundas<br>" +
+                "<a href='https://edisciplinas.usp.br/acessar/'>E-disciplinas</a>"
+    },
+    { 
+        materia: "Introdução à Lógica Digital",
+        nome: "Denis Fernando Wolf", 
+        sala: "3-124",
+        email: "denis@icmc.usp.br",
+        online: "Atendimentos: Terça (8:10 e 10:10) + Aulas gravadas no <a href='https://drive.google.com/drive/u/1/folders/1E6-F7Y5x5EU16l9tdrPOEZ6P7vuMu23H'>Drive</a><br>" + 
+                "<a href='https://ae4.tidia-ae.usp.br/portal'>Tidia</a>"
+    },
+    { 
+        materia: "Eletrônica para Computação",
+        nome: "Eduardo do Valle Simões", 
+        sala: "3-134",
+        email: "simoes@icmc.usp.br",
+        online: "<a href='https://gitlab.com/simoesusp/disciplinas/-/tree/master/SSC0180-Eletronica-para-Computacao'>GitLab</a>"
+    },
+    { 
+        materia: "Geometria Analítica",
+        nome: "Farid Tari", 
+        sala: "4-212",
+        email: "faridtari@icmc.usp.br",
+        online: "Atendimento: Segunda 15h + Aulas gravadas no <a href='https://ae4.tidia-ae.usp.br/portal'>Tidia</a>"
+    },
+    { 
+        materia: "Cálculo I",
+        nome: "Ali Tahzibi", 
+        sala: "3-244",
+        email: "tahzibi@icmc.usp.br",
+        online: "Atendimentos: Quarta e Sexta 10h<br>" +
+                "<a href='https://ae4.tidia-ae.usp.br/portal'>Tidia</a>"
+    },
+    { 
+        materia: "Informação Profissional e Tutoria Acadêmica em Ciências da Computação",
+        nome: "João do Espírito Santo Batista Neto", 
+        sala: "4-222",
+        email: "jbatista@icmc.usp.br",
+        online: "Atendimentos: Quarta(irregular)<br>" +
+                "<a href='https://ae4.tidia-ae.usp.br/portal'>Tidia</a>"
+    },
+    { 
+        materia: "Laboratório de Introdução à Ciência de Computação I",
+        nome: "Moacir Antonelli Ponti", 
+        sala: "4-232 A",
+        email: "moacir@icmc.usp.br",
+        online: "Aulas: Quinta (14:30 - Moacir), ?(xx:xx - Leo)<br>" +
+                "<a href='https://ae4.tidia-ae.usp.br/portal'>Tidia</a><br>" +
+                "Avaliações: <a href='run.codes'>run.codes</a> - EX. 15-26 (entrega 13/04)" +
+                "<a href='https://t.me/joinchat/Ch9SmR2RHpkx7Mk_iIEZMw'>Grupo do Telegram</a>"
+    },
+    { 
+        materia: "Prática em Lógica Digital",
+        nome: "Alexandre Cláudio Botazzo Delbem", 
+        sala: "4-108",
+        email: "acbd@icmc.usp.br",
+        online: "Atendimentos: Terça 14h<br>" +  
+                "<a href='https://edisciplinas.usp.br/acessar/'>E-disciplinas</a><br>" +
+                "Avaliações: Avalição e presença serão feitos através da entrega dos trabalhos. Os trabalhos estão disponíveis no <a href='https://edisciplinas.usp.br/acessar/'>E-disciplinas</a><br>" +
+                "Entregar os trabalhos no <a href='https://drive.google.com/drive/u/0/folders/1oKTfVuw25_n2rudS-_MSfKWtN9ZxdhNX'>Drive</a><br>" +
+                "<a href='https://chat.whatsapp.com/FFkymHtTXUP2z9PFhVHfVG'>Grupo WhatsApp</a>"
+    }
+];
+
+var atividades = [
+    {
+        dia: '28/03',
+        atividade: 'Ex. Cifra de César ICC no <a href="run.codes">run.codes</a>'
+    },
+    {
+        dia: '02/04',
+        atividade: 'Entrega Trabalho 3 Prática em LD'
+    },
+    {
+        dia: '03/04',
+        atividade: 'Prova GA (8h-10h)'
+    },
+    {
+        dia: '13/04',
+        atividade: 'Ex. 15 ao 21 LabICC no <a href="run.codes">run.codes</a>'
+    },
+    {
+        dia: '24/04',
+        atividade: 'Ex. Bar Plot ICC no <a href="run.codes">run.codes</a>'
+    }
+]
